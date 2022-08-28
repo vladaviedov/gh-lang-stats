b6cd7d46c82478417c8f0074a01fe79ab7fd24c0
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-08-28 09:28:09 -0400

    cleaned up

--- index.js
+++ index.js
@@ -5,26 +5,14 @@ const octokit = new Octokit({
 	auth: process.env.ACCESS_KEY
 });
 
-const languageMap = new Map();
-
 qlUserId(octokit).then(response => {
 	return qlUserCommits(octokit, response.viewer.id);
 }).then(response => {
 	const repos = response.viewer.repositoriesContributedTo.nodes;
 	return getData(repos);
 }).then(arr => {
-	arr.forEach(x => {
-		const files = x.data.files;
-		files.forEach(f => {
-			const name = f.filename;
-			if (name.indexOf(".") < 0 && name[0] != ".") return;
-			const ext = name.split(".").pop();
-			languageMap.set(ext, languageMap.has(ext) ? 
-				languageMap.get(ext) + f.changes :
-				f.changes);
-		});
-	});
-	console.log(languageMap);
+	console.log(arr);
+	// TODO: do things with data
 });
 
 const getData = repos => {
