009064ecb687f4a4615d4bdbfc469ccfbf9804bf
Vladyslav Aviedov <vladaviedov@protonmail.com>
2023-01-06 14:02:03 -0500

    added new config options

--- src/config.js
+++ src/config.js
@@ -3,5 +3,8 @@ export const config = {
 	maxConcur: process.env.MAX_CONCUR ?? 5,
 	inputFile: process.env.INPUT_FILE ?? "template.svg",
 	outputFile: process.env.OUTPUT_FILE ?? "generated.svg",
-	storeFile: process.env.STORE_FILE ?? "data.json"
+	storeFile: process.env.STORE_FILE ?? "data.json",
+	delta: process.env.DELTA ?? true,
+	deltaSeparate: process.env.DELTA_SEPARATE ?? false,
+	deltaSeparateRange: process.env.DELTA_SEPARATE_RANGE ?? 86400
 };
